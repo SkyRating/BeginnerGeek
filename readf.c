221beb8b796d2f980e0b36c5d3cc806b1dfc0048
Nord <43596475+SkyRating@users.noreply.github.com>
2018-09-29 15:07:11 +0300

    Update readf.c

--- readf.c
+++ readf.c
@@ -1,23 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
-int a,b;
+char b;
+char vowels[]={'A','a','E','e','I','i','O','o','U','u'};
+char cons[]={'B','b','C','c','D','d','F','f','G','g','H','h','J','j','K','k','L','l','M','m','N','n','P','p','Q','q','R','r','S','s','T','t','V','v','W','w','X','x','Y','y','Z','z'};
  FILE *file;
- file=fopen("fscanf.txt","rt");
- if((file=fopen("fscanf.txt","rt"))==NULL){
+ file=fopen("Arr.c","rt");
+ if((file=fopen("Arr.c","r"))==NULL){
  perror("None");
  exit(0);
  }
- int counter=0;
- while(fscanf(file,"%d",&a)!=EOF){
- printf("Clean ");
- if(a==1){
- printf("\n");
+int vow=0;
+int con=0;
+while(fscanf(file,"%c",&b)!=EOF){
+ for(int v=0; v<=9; v++){
+  if(b==vowels[v]){
+  vow++;
+  break;
+  }
+  }                               //check
+ for(int c=0;c<=41;c++){
+  if(b==cons[c]){
+   con++;
+    break;
+   }
+  }
+ if((b=='{')||(b=='}')){
  continue;
  }
- printf("%d \n",a);
- counter++;
+ if((b=='\n')||(b=='\0')){
+  if((vow==0) && (con==0)){
+   printf(" Only signs included\n");
+  }else{
+  printf("   (Vowels=%d Consonants=%d)\n",vow,con);
+  vow=0;
+  con=0;
  }
- printf("Number of symbols %d", counter);
+ }else{
+ printf("%c",b);
+} 
+}
  fclose(file);
 }
